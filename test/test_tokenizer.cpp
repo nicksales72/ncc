8008a2dc055bb487dbf2889e5298060333555c60
nicksales72 <nicksales72@gmail.com>
2026-07-09 22:54:53 -0400

    a slightly bigger test

--- test/test_tokenizer.cpp
+++ test/test_tokenizer.cpp
@@ -20,3 +20,17 @@ TEST(TokenizerTest, TestExample1) {
    
     EXPECT_EQ(tokenizer.tokenized_file, expected_vector);
 }
+
+
+TEST(TokenizerTest, TestExample2) {
+    std::vector<TokenType> expected_vector = {TOKEN_INT, TOKEN_IDENTIFIER, TOKEN_LEFT_PAREN, TOKEN_INT,
+                                              TOKEN_IDENTIFIER, TOKEN_RIGHT_PAREN, TOKEN_LEFT_BRACE, 
+                                              TOKEN_RETURN, TOKEN_IDENTIFIER, TOKEN_SEMICOLON, TOKEN_RIGHT_BRACE,
+                                              TOKEN_INT, TOKEN_IDENTIFIER, TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,  
+                                              TOKEN_LEFT_BRACE, TOKEN_INT, TOKEN_IDENTIFIER, TOKEN_SEMICOLON,
+                                              TOKEN_IDENTIFIER, TOKEN_LEFT_PAREN, TOKEN_IDENTIFIER, TOKEN_RIGHT_PAREN,
+                                              TOKEN_SEMICOLON, TOKEN_RETURN, TOKEN_INT_LIT, TOKEN_SEMICOLON, TOKEN_RIGHT_BRACE};
+    Tokenizer tokenizer("examples/example2.c");
+   
+    EXPECT_EQ(tokenizer.tokenized_file, expected_vector);
+}

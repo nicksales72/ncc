19ae61c8c9f22c40f0fae30771766d15bff9c9d9
nicksales72 <nicksales72@gmail.com>
2026-08-02 16:56:08 -0400

    move ast structs to ast header

---
+++ src/parser/ast.hpp
@@ -0,0 +1,36 @@
+#pragma once 
+
+#include <iostream>
+
+struct Exp {
+    int value{};   
+
+    friend std::ostream& operator<<(std::ostream& os, const Exp& exp) {
+        return os << "Exp(" << exp.value << ")";
+    }
+};
+
+struct Statement {
+    Exp return_expression;
+
+    friend std::ostream& operator<<(std::ostream& os, const Statement& statement) {
+        return os << "Statement(" << statement.return_expression << ")";
+    }
+};
+
+struct Function {
+    std::string function_name;
+    Statement function_statement;
+
+    friend std::ostream& operator<<(std::ostream& os, const Function& function) {
+        return os << "Function(Name: " << function.function_name << ", " << function.function_statement << ")";
+    }
+};
+
+struct Program {
+    Function program_function;
+
+    friend std::ostream& operator<<(std::ostream& os, const Program& program) {
+        return os << "Program(" << program.program_function << ")" << '\n';
+    }
+};
--- src/tokenizer/tokenizer.hpp
+++ src/tokenizer/tokenizer.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "token.hpp"
+#include "../parser/ast.hpp"
 
 class Tokenizer {
 public: 
@@ -15,39 +16,6 @@ public:
 
 private:
     std::deque<Token> tokenizeFile(const std::vector<char>&);
-};
-
-struct Exp {
-    int value{};   
-
-    friend std::ostream& operator<<(std::ostream& os, const Exp& exp) {
-        return os << "Exp(" << exp.value << ")";
-    }
-};
-
-struct Statement {
-    Exp return_expression;
-
-    friend std::ostream& operator<<(std::ostream& os, const Statement& statement) {
-        return os << "Statement(" << statement.return_expression << ")";
-    }
-};
-
-struct Function {
-    std::string function_name;
-    Statement function_statement;
-
-    friend std::ostream& operator<<(std::ostream& os, const Function& function) {
-        return os << "Function(Name: " << function.function_name << ", " << function.function_statement << ")";
-    }
-};
-
-struct Program {
-    Function program_function;
-
-    friend std::ostream& operator<<(std::ostream& os, const Program& program) {
-        return os << "Program(" << program.program_function << ")" << '\n';
-    }
 };
 
 Program parseTokens(std::deque<Token>& tokenized_file);
